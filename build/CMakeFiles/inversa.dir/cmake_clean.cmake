567aa514d8bf0dc72be9ef7161f48ddef4fbb6f1
joigs <jose.saez2001@alumnos.ubiobio.cl>
2025-10-07 23:37:01 -0300

    a

---
+++ build/CMakeFiles/inversa.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inversa.dir/src/inversa.cpp.o"
+  "CMakeFiles/inversa.dir/src/inversa.cpp.o.d"
+  "inversa"
+  "inversa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inversa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/original.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/original.dir/src/original.cpp.o"
+  "CMakeFiles/original.dir/src/original.cpp.o.d"
+  "original"
+  "original.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/original.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
